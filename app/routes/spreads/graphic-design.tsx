59d357257e437f17ed8603c0436d05aa13889453
Ivan Mijic <ivan@epifront.com>
2025-08-10 20:33:38 +0200

    {@IvanMijic0}[main]{feat): Graphic Design 2.

--- app/routes/spreads/graphic-design.tsx
+++ app/routes/spreads/graphic-design.tsx
@@ -26,7 +26,7 @@ export const Right = forwardRef<HTMLDivElement>( ( _, ref ) => (
 		/>
 
 		<article
-			className="absolute inset-0 p-12 z-20 h-full w-full text-black grid grid-rows-[1fr_auto_1fr] gap-6"
+			className="absolute inset-0 pr-12 2xl:pr-20 pt-28 2xl:pt-36 pb-12 z-20 h-full w-full text-black gap-6"
 			itemScope
 			itemType="https://schema.org/CreativeWork"
 			itemID="/book/graphic-design"
@@ -40,37 +40,40 @@ export const Right = forwardRef<HTMLDivElement>( ( _, ref ) => (
 			<meta itemProp="image" content="/graphic-design-right.webp"/>
 			<meta itemProp="image" content="/graphic-design-right-cards.webp"/>
 			<link itemProp="mainEntityOfPage" href="/book/graphic-design"/>
-			<div className="row-start-2 pb-20 2xl:pb-52 justify-self-center flex flex-col items-center gap-5 2xl:gap-8">
-				<header className="flex flex-col gap-8">
-					<h1
-						className="text-[6rem] text-right 2xl:text-[8rem] leading-22 2xl:leading-30 text-[#363636] [-webkit-text-stroke:2px_#363636] italic [text-stroke:2px_#363636]"
-						itemProp="headline"
+			<div className="contents">
+				<div
+					className="justify-self-center h-full flex flex-col items-center gap-5 2xl:gap-8">
+					<header className="flex flex-col gap-8">
+						<h1
+							className="text-[6rem] text-right 2xl:text-[8rem] leading-22 2xl:leading-28 text-[#363636] [-webkit-text-stroke:2px_#363636] [text-stroke:2px_#363636]"
+							itemProp="headline"
+						>
+							GRAPHIC<br/>DESIGN
+						</h1>
+						<p className="sr-only">
+						  <span itemProp="author" itemScope itemType="https://schema.org/Person">
+							<span itemProp="name">Amna Kolić</span>
+						  </span>
+						</p>
+					</header>
+					<section
+						className="flex flex-col w-full h-full  font-serif items-end justify-between text-[#505050] gap-4"
+						itemProp="description"
 					>
-						GRAPHIC<br/>DESIGN
-					</h1>
-					<p className="sr-only">
-            <span itemProp="author" itemScope itemType="https://schema.org/Person">
-              <span itemProp="name">Amna Kolić</span>
-            </span>
-					</p>
-				</header>
-				<section
-					className="flex flex-col w-full font-serif items-end justify-between text-[#505050] gap-4"
-					itemProp="description"
-				>
-					<p className="w-64 2xl:w-[19rem] font-bold italic text-justify text-base 2xl:text-lg">
-						Where creativity and intention intertwine, designs unfold stories through subtle details and
-						refined aesthetics. From elegant identities to vibrant narratives and tactile impressions, each
-						creation captures a unique essence—inviting you to experience the art behind the visuals.
-					</p>
-				</section>
-			</div>
-			<div className="row-start-3 self-end justify-self-end w-full flex justify-end">
-				<p className="w-72 2xl:w-[16rem] font-serif text-xs 2xl:text-sm text-right font-bold">
-					Background photograph: Amna Kolić<br/>
-					Cards photograph: Freepik<br/>
-					Edit &amp; Logos: Amna Kolić
-				</p>
+						<p className="w-56 font-bold 2xl:w-[19rem] italic text-justify text-base 2xl:text-lg">
+							Where creativity and intention intertwine, designs unfold stories through subtle details and
+							refined aesthetics.
+							From elegant identities to vibrant narratives and tactile impressions, each creation
+							captures a unique
+							essence—inviting you to experience the art behind the visuals.
+						</p>
+						<p className="text-[#505050] font-serif text-xs 2xl:text-sm text-right font-bold">
+							Background photograph: Amna Kolić<br/>
+							Cards photograph: Freepik<br/>
+							Edit &amp; Logos: Amna Kolić
+						</p>
+					</section>
+				</div>
 			</div>
 		</article>
 		<img
