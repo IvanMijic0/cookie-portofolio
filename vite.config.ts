ba7e2996ac577adc12db0513489a3a828b718669
Ivan Mijic <ivan@natively.dev>
2026-06-07 19:27:22 +0200

    build: disable production source maps to secure code and remove build warnings

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ isSsrBuild }) => ({
+export default defineConfig(({ mode, isSsrBuild }) => ({
 	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 	build: {
-		sourcemap: !isSsrBuild,
+		sourcemap: mode === "development" && !isSsrBuild,
 	},
 }));
