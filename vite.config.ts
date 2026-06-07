8f02dc61b8e8944b26077073de1e463e223aafc4
Ivan Mijic <ivan@natively.dev>
2026-06-07 18:36:41 +0200

    chore: conditionally disable sourcemaps for SSR server bundle

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(({ isSsrBuild }) => ({
 	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 	build: {
-		sourcemap: true,
+		sourcemap: !isSsrBuild,
 	},
-});
+}));
